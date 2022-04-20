213ac219a07fd7906e89809b13930f847e8a67ce
GerardoSL1803 <gerardosolano93@gmail.com>
2022-04-20 14:47:30 -0600

    Actulizacion de estructura de api

---
+++ SistemaUniversidad.BackEnd.BD/SistemaUniversidad.BackEnd.API/Repository/IObtenerRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaUniversidad.BackEnd.API.Repository
+{
+    public interface IObtenerRepository<T,Y> where T : class 
+    {
+        List<T> SeleccionarTodos();
+        T SeleccionarPorId(Y Id);
+    }
+    
+}
---
+++ SistemaUniversidad.BackEnd.BD/SistemaUniversidad.BackEnd.API/UnitOfWork.SqlServer/UnitOfWorkSqlServerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace SistemaUniversidad.BackEnd.API.UnitOfWork.SqlServer
+{
+    public class UnitOfWorkSqlServerRepository : IUnitOfWorkRepository
+    {
+
+        public UnitOfWorkSqlServerRepository(SqlConnection context, SqlTransaction transaction) 
+        { 
+        
+        }
+
+    }
+}
