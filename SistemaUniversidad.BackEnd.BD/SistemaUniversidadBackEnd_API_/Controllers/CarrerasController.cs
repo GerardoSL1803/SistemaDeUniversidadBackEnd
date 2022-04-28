33fc215d8d16f3c806a276e20b1c9f35d5c9789a
GerardoSL1803 <gerardosolano93@gmail.com>
2022-04-28 00:19:10 -0600

    UPDATE

--- SistemaUniversidad.BackEnd.BD/SistemaUniversidadBackEnd_API_/Controllers/CarrerasController.cs
+++ SistemaUniversidad.BackEnd.BD/SistemaUniversidadBackEnd_API_/Controllers/CarrerasController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SistemaUniversidad.BackEnd.API.Dtos;
 using SistemaUniversidad.BackEnd.API.Models;
-using SistemaUniversidad.BackEnd.API.Services;
+using SistemaUniversidad.BackEnd.API.Services.Interfaces;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
